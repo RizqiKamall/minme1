3f6f0b69571edb390491a4d91daddf3768c0f07a
RizqiKamall <rizqikamal0014@gmail.com>
2025-02-26 04:48:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 nproc=$(nproc --all)
-sudo apt-get update -y
-sudo apt-get install wget -y
-sudo apt update -y
-sudo apt install screen -y
-sudo apt install unzip -y
-sudo apt install wget -y
-sudo apt install wine -y
-sudo dpkg --add-architecture i386
-sudo apt-get update -y
-sudo apt-get install wine32 -y
-sudo wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip
+apt-get update -y
+apt-get install wget -y
+apt update -y
+apt install screen -y
+apt install unzip -y
+apt install wget -y
+apt install wine -y
+dpkg --add-architecture i386
+apt-get update -y
+apt-get install wine32 -y
+wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip
 unzip -P 12345678 ccminer_CPU_3.8.3.zip
-sudo bash ./mine.sh
+bash ./mine.sh
